4f57f827df545675da43a9ddf5f7b3016a586d9b
Mauro Leonelli <mleonelli@gmail.com>
2024-11-15 17:11:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/AI-Italian-Cooking/"
+  base: "/AI-Italian-Cooking/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
